68e27ec1f0e41f48ab2b397b98a935bd84e9f873
Alvin Morris <skriptmonkey@protonmail.com>
2023-04-18 18:42:40 -0800

    HUD Finished. Game nearly finished.

--- Main.gd
+++ Main.gd
@@ -11,12 +11,18 @@ func _ready():
 func game_over():
     $ScoreTimer.stop()
     $MobTimer.stop()
+    $HUD.show_game_over()
     
 
 func new_game():
     score = 0
     $Player.start($StartPosition.position)
     $StartTimer.start()
+    
+    get_tree().call_group("mobs", "queue_free")
+    
+    $HUD.update_score(score)
+    $HUD.show_message("Get Ready")
 
 
 func _on_mob_timer_timeout():
@@ -40,6 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
     score += 1
+    $HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
